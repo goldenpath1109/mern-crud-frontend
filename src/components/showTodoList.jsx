4c91d102c0d94126a4bb7719dfd51efa82f10734
brilliantsunlight1109 <brilliantsunlight1109@gmail.com>
2023-11-01 21:10:48 +0900

    todolist frontend complete

--- src/components/showTodoList.jsx
+++ src/components/showTodoList.jsx
@@ -8,8 +8,8 @@ function TodoCard({ data, handleEdit, handleDelete }) {
 	return (
 		<li key={_id}>
 			<div className="title-description">
-				<h3>{ title }</h3>
-				<p>{ description }</p>
+				<h3>{title}</h3>
+				<p>{description}</p>
 			</div>
 
 			<div className="button-container">
@@ -21,34 +21,34 @@ function TodoCard({ data, handleEdit, handleDelete }) {
 				</button>
 			</div>
 		</li>
-	)
+	);
 }
 
-export function ShowTodoList(){
+export function ShowTodoList() {
 	const [todo, setTodo] = useState([]);
-	const [open, setOpen] = useState(false)
-	const [id, setId] = useState("")
+	const [open, setOpen] = useState(false);
+	const [id, setId] = useState("");
 	const [update, setUpdate] = useState(false);
 
 	useEffect(
-		function(){
+		function () {
 			axios
 				.get("http://localhost:8000/api/todo")
-				.then((res) =>{
+				.then((res) => {
 					console.log(res.data);
 					setTodo(res.data);
 				})
-				.catch((err) =>{
+				.catch((err) => {
 					console.log(err.message);
-				})
+				});
 		},
 		[update]
-	)
+	);
 	function handleEdit(e) {
 		setId(e.target.name);
 		setOpen(true);
 	}
-	function handleUpdate(){
+	function handleUpdate() {
 		console.log("update:", update, !update);
 		setUpdate(!update);
 	}
@@ -57,7 +57,7 @@ export function ShowTodoList(){
 
 		setTodo((data) => {
 			return data.filter((todo) => todo._id !== e.target.name);
-		})
+		});
 	}
 	function handleClose() {
 		setId("");
@@ -72,26 +72,31 @@ export function ShowTodoList(){
 			<section className="contents">
 				<h1>TODO</h1>
 				<ul className="list-container">
-					{
-						todo.map((data) =>(
-							<TodoCard data={data} handleEdit={handleEdit} handleDelete={handleDelete} />
-						))
-					}
+					{todo.map((data) => (
+						<TodoCard
+							data={data}
+							handleEdit={handleEdit}
+							handleDelete={handleDelete}
+						/>
+					))}
 				</ul>
 				//added
-				{
-					open ? (
-						<section className="update-container">
-							<div className="update-contents">
-								<p onClick={handleClose} className="close">
-									&times;
-								</p>
-								<UpdateTodo _id={id} handleClose={handleClose} handleUpdate={handleUpdate}
-								/>
-							</div>
-						</section>
-					) : ("")
-				}
+				{open ? (
+					<section className="update-container">
+						<div className="update-contents">
+							<p onClick={handleClose} className="close">
+								&times;
+							</p>
+							<UpdateTodo
+								_id={id}
+								handleClose={handleClose}
+								handleUpdate={handleUpdate}
+							/>
+						</div>
+					</section>
+				) : (
+					""
+				)}
 			</section>
 		</section>
 	);
